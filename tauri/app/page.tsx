9306239efa415d46ee2dec4fbb1ce9f01f1fec78
lukascobit <lukyn.odehnal333@gmail.com>
2024-08-01 21:51:29 +0200

    get subjects

--- tauri/app/page.tsx
+++ tauri/app/page.tsx
@@ -1,24 +1,68 @@
-"use client"
-import Image from "next/image";
-import { useState } from 'react';
-import logo from "../public/vercel.svg";
+"use client";
+// import Image from "next/image";
+import { useEffect, useState } from "react";
+import { invoke } from "@tauri-apps/api/tauri";
+// import logo from "../public/vercel.svg";
 
 export default function Home() {
-  const [username, setUsername] = useState("")
-  const [password, setPassword] = useState("")
+  const [greeting, setGreeting] = useState("");
+  const [grades, setGrades] = useState("");
+  const [subjects, setSubjects] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    invoke<string>("greet", { name: "Next.js" })
+      .then((result) => setGreeting(result))
+      .catch(console.error);
+  }, []);
+
+  function signin(e: any) {
+    e.preventDefault();
+    console.log(`sign in ${username} ${password}`);
+    invoke<string>("subjects", { username, password })
+      .then((result) => setSubjects(result))
+      .catch(console.error);
+    console.log(subjects);
+  }
+  function chooseSubject(d: string) {
+    invoke<string>("grades", { subject: d })
+      .then((result) => setGrades(result))
+      .catch(console.error);
+    console.log(subjects);
+  }
   return (
     <main className="content ">
-      <form className="signin">
+      <h1>{grades}</h1>
+      <form
+        className={grades || subjects ? "none" : "signin"}
+        onSubmit={(e) => signin(e)}
+      >
         <label>Username</label>
-        <br/>
-        <input value={username} onChange={(e)=>setUsername(e.target.value)}/>
-        <br/>
+        <br />
+        <input value={username} onChange={(e) => setUsername(e.target.value)} />
+        <br />
         <label>Password</label>
-        <br/>
-        <input value={password} onChange={(e)=>setPassword(e.target.value)}/>
-        <br/>
-        <button>Sign in</button>
+        <br />
+        <input value={password} onChange={(e) => setPassword(e.target.value)} />
+
+        <br />
+        <button className="signButton">Sign in</button>
       </form>
+      {/*<h1 className="header">Choose a subject: </h1>*/}
+      <div className={subjects ? "subjects" : "none"}>
+        {subjects.split(",").map((d) => {
+          return (
+            <button
+              key={d}
+              onClick={() => chooseSubject(d)}
+              className="subject"
+            >
+              {d}
+            </button>
+          );
+        })}
+      </div>
     </main>
   );
 }
--- tauri/src-tauri/src/main.rs
+++ tauri/src-tauri/src/main.rs
@@ -1,8 +1,227 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-fn main() {
+use colored::Colorize;
+use headless_chrome::Browser;
+use std::{collections::HashSet, env, error::Error, io, io::prelude::*, time::Duration};
+
+const WAIT_LIMIT: u64 = 15;
+struct ZnamkaStruct {
+    predmet: String,
+    znamka: f32,
+    vaha: f32,
+}
+
+// the individual processing functions
+fn process_two(znamka: &str) -> Option<f32> {
+    // 2- etc.
+    let returned = znamka.chars().nth(0).unwrap().to_string();
+    let returned_num: f32 = returned.parse().expect("a proper float");
+    // println!("{:?}", returned_num);
+    Some(returned_num + 0.5)
+}
+fn process_longer(znamka: &str) -> Option<f32> {
+    // 9 / 15 = 60% → 3 etc.
+    let returned = znamka.chars().nth_back(0).unwrap().to_string();
+    let returned_num: f32 = returned.parse().expect("a proper float");
+    Some(returned_num)
+}
+fn process_percent(znamka: &str) -> Option<f32> {
+    // 100% etc.
+    let znamka: f32 = znamka.replace("%", "").parse().expect("a proper float");
+    Some(znamka)
+}
+fn prompt_and_read(prompt: &str) -> Result<usize, Box<dyn std::error::Error>> {
+    print!("{}", prompt);
+    io::stdout().flush()?;
+    let mut input = String::new();
+    io::stdin().lock().read_line(&mut input)?;
+    Ok(input.trim().parse()?)
+}
+fn prompt_and_read_creds(prompt: &str) -> String {
+    print!("{}", prompt);
+    io::stdout().flush().expect("failed to flush stdout");
+    let mut input = String::new();
+    io::stdin()
+        .lock()
+        .read_line(&mut input)
+        .expect("failed to read line from user");
+    input.trim().to_string()
+}
+
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}!", name)
+}
+#[tauri::command]
+fn subjects(username: &str, password: &str) -> String {
+    format!("Hello, {}{}!", username, password);
+    match get_subjects(username, password) {
+        Ok(d) => {
+            println!("success");
+            d
+        }
+        Err(_) => {
+            println!("bad data");
+            String::from("could not get grades")
+        }
+    }
+}
+
+fn get_subjects(username: &str, password: &str) -> Result<String, Box<dyn Error>> {
+    let browser = Browser::default()?;
+    let tab = browser.new_tab()?;
+
+    loop {
+        tab.navigate_to("https://sspbrno.edupage.org/login/edubarLogin.php")?;
+        tab.wait_for_element("input#home_Login_2e1")?.click()?;
+        tab.type_str(&username)?;
+        tab.wait_for_element("input#home_Login_2e2")?.click()?;
+        tab.type_str(&password)?.press_key("Enter")?;
+
+        match tab.wait_for_element(".user-button-icon-outer") {
+            Ok(_) => {
+                println!("Signed in successfully!")
+            }
+            Err(_) => {
+                println!("Wrong credentials");
+                let _ = main();
+            }
+        };
+        // println!("finished signin");
+
+        tab.navigate_to("https://sspbrno.edupage.org/znamky/?eqa=d2hhdD1zdHVkZW50dmlld2VyJnBvaGxhZD1wb2RsYURhdHVtdSZ6bmFta3lfeWVhcmlkPTIwMjMmem5hbWt5X3llYXJpZF9ucz0xJm5hZG9iZG9iaWU9UDImcm9rb2Jkb2JpZT0yMDIzJTNBJTNBUDImZG9ScT0xJndoYXQ9c3R1ZGVudHZpZXdlciZ1cGRhdGVMYXN0Vmlldz0w")?;
+        println!("Getting the grades...");
+        match tab.wait_for_element_with_custom_timeout(
+            "#edubarStartButton",
+            Duration::from_secs(WAIT_LIMIT),
+        ) {
+            Ok(_) => {
+                break;
+            }
+            Err(_) => {
+                println!("Grades not found; trying again");
+                let _ = main();
+            }
+        };
+    }
+    let mut everything_vec: Vec<ZnamkaStruct> = vec![];
+    let everything = tab.find_elements(".app-list-item-main")?;
+
+    for i in everything {
+        let inner_text = i.get_inner_text()?;
+        let all: Vec<&str> = inner_text.lines().collect();
+
+        // println!("{:?}", all);
+        let new_znamka = all[2];
+        if all[0] == "Chování" || all[1] == "Vysvědčení" {
+            continue;
+        }
+
+        let created_znamka = match new_znamka.parse::<f32>() {
+            Ok(znamka_int) => {
+                println!("Parsed grade: {}", znamka_int.to_string().green());
+                Ok(znamka_int)
+            }
+            Err(_) => {
+                // println!("'{}' není v normálním formátu", new_znamka.yellow());
+                if new_znamka.chars().nth_back(0) == Some('%') {
+                    let extracted_znamka = process_percent(&new_znamka);
+                    if extracted_znamka.is_some() {
+                        Ok(extracted_znamka.expect("adding a working grade failed"))
+                    } else {
+                        Err("extracted_znamka doesnt exist")
+                    }
+                } else {
+                    // println!("new_znamka: {:?}", &new_znamka);
+                    let extracted_znamka = match new_znamka.len() {
+                        1 => {
+                            // println!("+/-/o/S se nevztahuje na prumer");
+                            None
+                        }
+                        2 => process_two(&new_znamka),
+                        3.. => process_longer(&new_znamka),
+                        _ => {
+                            println!("the length of grade is 0 or negative!");
+                            None
+                        }
+                    };
+                    if extracted_znamka.is_none() {
+                        Err("this grade was not counted into the average")
+                    } else {
+                        // println!("{:?}", extracted_znamka);
+                        Ok(extracted_znamka.expect("adding a working grade failed"))
+                    }
+                }
+            }
+        };
+        match created_znamka {
+            Ok(created_znamka) => {
+                if let Some(start) = all[1].find('∙') {
+                    if let Some(end) = all[1].find('×') {
+                        let start_pos = start + '∙'.len_utf8();
+                        let result = &all[1][start_pos..end].trim();
+                        let vaha = result.parse().unwrap();
+                        // println!("vaha: {}", vaha);
+                        let new_znamka_instance = ZnamkaStruct {
+                            predmet: all[0].to_string(),
+                            znamka: created_znamka,
+                            vaha,
+                        };
+                        // println!("vaha saved: {}", new_znamka_instance.vaha);
+                        // println!("znamka saved: {}", new_znamka_instance.znamka);
+                        everything_vec.push(new_znamka_instance);
+                    } else {
+                        println!("The '×' character was not found.");
+                    }
+                } else {
+                    println!("The '∙' character was not found.");
+                }
+            }
+            Err(_) => {
+                // println!("error: the created_znamka {:?} isn't valid", created_znamka);
+            }
+        };
+        // println!("{:?}", created_znamka);
+    }
+    print!("{}[2J", 27 as char); //clear the screen
+    let mut set_existujicich_predmetu = HashSet::new();
+    for i in &everything_vec {
+        // println!("{:?}", &i);
+        set_existujicich_predmetu.insert(&i.predmet);
+    }
+    let mut vec_predmetu = Vec::from_iter(set_existujicich_predmetu);
+    vec_predmetu.sort();
+    for (i, p) in vec_predmetu.clone().into_iter().enumerate() {
+        println!("{}) - {}", i, p);
+    }
+    Ok(vec_predmetu
+        .iter()
+        .map(|x| x.to_string() + ",")
+        .collect::<String>())
+}
+
+pub fn main() {
     tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet, subjects])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+
+    // match dotenv() {
+    //     Ok(..) => {
+    //         println!(".env found, signing in...");
+    //         username = env::var("USERNAME").expect("USERNAME environment variable not set");
+    //         password = env::var("PASSWORD").expect("PASSWORD environment variable not set");
+    //     }
+    //     Err(..) => {
+    //         println!(".env not found, sign in manually: ");
+    //         username = prompt_and_read_creds("Your EduPage username: ").to_string();
+    //
+    //         print!("Your EduPage password: ");
+    //         io::stdout().flush().expect("failed to flush stdout");
+    //         password = rpassword::read_password().unwrap();
+    //     }
+    // }
+
+    // Ok(())
 }
